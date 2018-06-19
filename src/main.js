0ecf818129a81a67fa2b8310024e7303aa159836
usbpendrive <ferry.fernando@gmail.com>
2018-06-19 09:24:51 +0700

    Add Kendo UI Vue Element

--- src/main.js
+++ src/main.js
@@ -3,11 +3,28 @@
 import Vue from 'vue'
 import App from './App'
 
+import {Button, ButtonsInstaller} from '@progress/kendo-buttons-vue-wrapper'
+import {Chart, ChartInstaller} from '@progress/kendo-charts-vue-wrapper'
+import {DatePicker, DateinputsInstaller} from '@progress/kendo-dateinputs-vue-wrapper'
+import {DropDownList, DropdownsInstaller} from '@progress/kendo-dropdowns-vue-wrapper'
+import '@progress/kendo-theme-material/dist/all.css'
+import '@progress/kendo-ui'
+
 Vue.config.productionTip = false
+Vue.use(ButtonsInstaller)
+Vue.use(ChartInstaller)
+Vue.use(DateinputsInstaller)
+Vue.use(DropdownsInstaller)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: {
+    App,
+    Button,
+    Chart,
+    DatePicker,
+    DropDownList
+  }
 })
